eaa8427607cb38b74651143c32f36d233b6b09cc
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-03-11 11:12:44 +0100

    added subject and isubject

---
+++ StockTrading/ISubject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading
+{
+    public interface ISubject<T> where T : class
+    {
+        void Attach(T observer);
+        void Detach(T observer);
+        void Notify();
+    }
+}
