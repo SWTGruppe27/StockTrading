0e2742ef170a90f078b5f93b3d61f80df424cfa6
Simon <simonkjaer449@gmail.com>
2021-03-11 11:08:55 +0100

    Made IObserver and PortfolioDisplay

---
+++ StockTrading/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading
+{
+    public interface IObserver
+    {
+        void Update();
+    }
+}
---
+++ StockTrading/PortfolioDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading
+{
+    public class PortfolioDisplay
+    {
+        //private ISubject _subject;
+
+        public void Update()
+        {
+
+        }
+    }
+}
